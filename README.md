2aa8c649b95a232fd079883378da8234935f8302
rupertoss <rakoski.rupertoss@gmail.com>
2017-12-20 18:33:25 +0100

    ver 06
    ex. 4

--- README.md
+++ README.md
@@ -38,3 +38,15 @@ Exercise 3. Successor with delete
  *  - Remove x from S
  *  - Find the successor of x: the smallest y in S such that y>=x.
  * design a data type so that all operations (except construction) should take logarithmic time or better.
+ <br>
+ 
+ Exercise 4. Percolation
+ *
+ * Given a N-by-N grid of sites. Each site is either open or blocked. 
+ * A full site is an open site that can be connected to an open site 
+ * in the top row via a chain of neighboring (left, right, up, down) open sites. 
+ * We say the system percolates if there is a full site in the bottom row. 
+ * In other words, a system percolates if we fill all open sites connected 
+ * to the top row and that process fills some open site on the bottom row.
+ * More details:<a href="http://coursera.cs.princeton.edu/algs4/assignments/percolation.html">Programming Assignment: Percolation</a>
+ <br>
--- src/exercises/Ex4Percolation.java
+++ src/exercises/Ex4Percolation.java
@@ -1,3 +1,40 @@
+/*
+ * Percolation - Monte Carlo simulation
+ * 
+ * http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
+ * 
+ * Given a N-by-N grid of sites. Each site is either open or blocked. 
+ * A full site is an open site that can be connected to an open site 
+ * in the top row via a chain of neighboring (left, right, up, down) open sites. 
+ * We say the system percolates if there is a full site in the bottom row. 
+ * In other words, a system percolates if we fill all open sites connected 
+ * to the top row and that process fills some open site on the bottom row.
+ * 
+ * The solution is based on WeightedQuickUnionPathCompression algorithm.
+ * 
+ * ==========================================
+ * Exemplary array (size = 5) of sites looks like:
+ * 			0				<- top site
+ * 1	2	3	4	5
+ * 6	7	8	9	10
+ * 11	12	13	14	15	
+ * 16	17	18	19	20
+ * 21	22	23	24	25
+ * 			26				<- bottom site
+ * 
+ * ==============================================
+ * Exemplary results 
+ * Enter the size of the block
+ * 1000
+ * Enter the number of runs
+ * 100
+ * 
+ * Perlocation stats >> block size: 1000 , number of runs: 100
+ * 
+ * Threshold = 0.59258189
+ * Stddev = 0.0030438897611280197
+ * 95% confidence interval = [0.5817682842616954, 0.6033954957383045]
+ */
 
 package exercises;
 
@@ -6,30 +43,39 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Percolation {
+public class Ex4Percolation {
 	
 	private boolean [] open;
 	
     private int[] parent;  // parent[i] = parent of i
     private int[] size;    // size[i] = number of sites in tree rooted at i
-    private int count;     // number of components
+    private int count;     // number of sites opened
+	
+    /**
+     * Runs the percolation test on block with the size {@code size}
+     * 
+     * @param size the integer representing size of the block
+     */
+	private Ex4Percolation(int size) {
 		
-	private Percolation(int size) {
+		//initializing arrays
 		open = new boolean[(size*size)+2];
         parent = new int[(size*size)+2];
         this.size = new int[(size*size)+2];
 
+        //setting default values
         for (int i = 0; i < open.length; i++) {
         	open[i] = false;
             parent[i] = i;
             this.size[i] = 1;
         }
+		count = 0;
 		
+        //top and bottom site is always open
 		open[0] = true;
 		open[open.length - 1] = true;
 		
-		count = 0;
-		
+		//processing opening sites until top and bottom is connected
 		do {
 			int random = ThreadLocalRandom.current().nextInt(1, (size*size)+1);
 			open(random, size);
@@ -38,25 +84,41 @@ public class Percolation {
 		
 	}
 	
+	/**
+     * Opens the site {@code site} (if not open already) 
+     * and connects to the adjacent open sites
+     * 
+	 * @param site the integer representing site
+	 * @param size the integer representing size of the block
+	 */
 	private void open(int site, int size) {
 		
+		//check if already open
 		if (open[site]) 
 			return;
 		
+		//opening
 		open[site] = true;
 		
+		//counting the number of open sites
+        count++;
+		
+		//connect to the west
 		if(site%size != 1) 
 			union(site, site - 1);
 		
+		//connect to the east
 		if(site%size != 0)
 			union(site, site + 1);
 		
+		//connect to the north, if first row connect to the top
 		if(!(site <= size)) {
 			union(site, site - size);
 		} else {
 			union(site, 0);
 		}
 		
+		//connect to the south, if last row connect to the bottom
 		if(!(site > (size - 1)*size)) {
 			union(site, site + size);
 		} else {
@@ -87,16 +149,14 @@ public class Percolation {
 	            parent[rootSite2] = rootSite1;
 	            size[rootSite1] += size[rootSite2];
 	        }
-	        count++;
     	}
     }
     
     /**
-     * Returns the component identifier for the component containing site {@code p}.
+     * Returns the component identifier for the component containing site {@code site}.
      *
      * @param  site the integer representing one site
-     * @return the component identifier for the component containing site {@code p}
-     * @throws IllegalArgumentException unless {@code 0 <= p < n}
+     * @return the component identifier for the component containing site {@code site}
      */
     public int find(int site) {
         int root = site;
@@ -121,9 +181,10 @@ public class Percolation {
         return find(0) == find(bottom);
     }
 
-	
+
 	public static void main (String[] args) {
 		
+		//getting user input 
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the size of the block");
 		int size = scan.nextInt();
@@ -131,35 +192,35 @@ public class Percolation {
 		int runs = scan.nextInt();
 		scan.close();
 		
- 
-		List<Percolation> percolations = new LinkedList<Percolation>();
+		//running the percolations
+		List<Ex4Percolation> percolations = new LinkedList<Ex4Percolation>();
 		for (int i=0; i < runs; i++) {
-			percolations.add(new Percolation(size));
+			percolations.add(new Ex4Percolation(size));
 		}
 		
+		//statistics data
 		double sum = 0;
-		double mean;
+		double thresholdMean;
 		double tmpStdDev = 0;
 		
-		for (Percolation per: percolations) {
-			sum += per.count;
+		for (Ex4Percolation per: percolations) {
+			sum += per.count/(double)(size*size);
 		}
 		
-		mean = sum/runs;
+		thresholdMean = sum/runs;
 		
-		for (Percolation per: percolations) {
-			tmpStdDev += Math.pow((double)per.count - mean, 2);
+		for (Ex4Percolation per: percolations) {
+			tmpStdDev += Math.pow((double)per.count/(double)(size*size) - thresholdMean, 2);
 		}
 		
 		double standardDeviation = Math.sqrt(tmpStdDev/runs);
-		double bottomBorder = mean - (1.96*Math.sqrt(standardDeviation))/Math.sqrt(runs);
-		double topBorder = mean + (1.96*Math.sqrt(standardDeviation))/Math.sqrt(runs);
+		double bottomBorder = thresholdMean - (1.96*Math.sqrt(standardDeviation))/Math.sqrt(runs);
+		double topBorder = thresholdMean + (1.96*Math.sqrt(standardDeviation))/Math.sqrt(runs);
 		
+		//printing results
 		System.out.printf("\nPerlocation stats >> block size: %d , number of runs: %d", size, runs);
-		System.out.println("\nTRESHOLD = " + (mean)/(size*size));
-		System.out.println("mean = " + mean);
-		System.out.println("stddev = " + standardDeviation);
+		System.out.println("\n\nThreshold = " + thresholdMean);
+		System.out.println("Stddev = " + standardDeviation);
 		System.out.println("95% confidence interval = [" + bottomBorder + ", " + topBorder + "]");
-		
 	}
 }
