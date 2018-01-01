57c53e48b54e8821df3acdec818df90ea5947af5
rupertoss <rakoski.rupertoss@gmail.com>
2018-01-01 23:54:29 +0100

    ver 15
    ex. 12

--- src/exercises/Ex12EggDrop.java
+++ src/exercises/Ex12EggDrop.java
@@ -17,7 +17,7 @@ public class Ex12EggDrop {
 		System.out.println("The eggs will break at: " + limit);
 		
 		//version 0: 1 egg, tosses <= @limit
-		//selection search
+		//sequential search
 		eggs = 0;
 		tosses = 0;
 		
@@ -66,32 +66,32 @@ public class Ex12EggDrop {
 			tosses++;
 			if(i-1 > limit) {
 				eggs++;
+			    lo = tosses/2;
+		        hi = floors;
+		        while (lo <= hi) {
+		        	tosses++;
+		            int mid = lo + (hi - lo) / 2;
+		            if (limit < mid) { 
+		            	hi = mid - 1;
+		            	eggs++;
+		            }
+		            else if (limit > mid) 
+		            	lo = mid + 1;
+		            else {
+		            	eggs++;
+		            	break;
+		            }
+		        }
 				break;
 			}
 		}
-		
-	    lo = tosses/2;
-        hi = floors;
-        while (lo <= hi) {
-        	tosses++;
-            int mid = lo + (hi - lo) / 2;
-            if (limit < mid) { 
-            	hi = mid - 1;
-            	eggs++;
-            }
-            else if (limit > mid) 
-            	lo = mid + 1;
-            else {
-            	eggs++;
-            	break;
-            }
-        }
+
 		System.out.println("Version 2");
 		System.out.println("Eggs used: " + eggs);
 		System.out.println("Tosses: " + tosses);
 		
 		//version 3: 2 eggs, 2sqrt(@floors) tosses
-		//divide into pieces containing sqrt(@floors) and then selection search
+		//divide into pieces containing sqrt(@floors) and then sequential search
 		eggs = 0;
 		tosses = 0;
 		
@@ -111,6 +111,31 @@ public class Ex12EggDrop {
 		System.out.println("Version 3");
 		System.out.println("Eggs used: " + eggs);
 		System.out.println("Tosses: " + tosses);
+		
+		//version 4: 2 eggs, c*sqrt(@limit) tosses for some fixed constant c
+		//1>2>3>4... intervals and the sequential search
+		
+		eggs = 0;
+		tosses = 0;
+		int interval = 0;
+		for(int i = 0; i < floors; i += interval) {
+			interval++;
+			tosses++;
+			if(i >= limit) {
+				eggs++;
+				i -= interval;
+				while(i < limit) {
+					tosses++;
+					i++;
+				}
+				eggs++;
+				break;
+			}
+		}
+		System.out.println("Version 4");
+		System.out.println("Eggs used: " + eggs);
+		System.out.println("Tosses: " + tosses);
+		
 	}
 	
 	public static void main(String[] args) {
